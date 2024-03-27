fdcf06f39cd801bd65dfc6161dacfc24ccf065d7
Dakota <dakota.spencer0327@gmail.com>
2024-03-27 10:21:13 -0600

    Fetching pokemon data now works

--- frontend/pokesite/src/pages/SelectedPokemon.js
+++ frontend/pokesite/src/pages/SelectedPokemon.js
@@ -1,16 +1,30 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router';
 import { useSearchParams } from 'react-router-dom';
+import axios from 'axios';
 
 const SelectedPokemon = () => {
     const [queryParameters] = useSearchParams()
     const [selectedPokemon, setSelectedPokemon] = useState('');
-
+    const [pokemonData, setPokemonData] = useState()
     useEffect(() => {
-        setSelectedPokemon(queryParameters.get("pokemon"))
+        setSelectedPokemon(queryParameters.get("pokemon"));
+        if (pokemonData === null || pokemonData === undefined) {
+            fetchPokemonData();
+        }
+        else{
+            console.log(pokemonData)
+        }
     })
+    useEffect(() => {
+        console.log(pokemonData)
+    }, [pokemonData])
+    const fetchPokemonData = async () => {
+        const results = await axios.get(`https://pokeapi.co/api/v2/pokemon/${selectedPokemon}`)
+        setPokemonData(results.data);
+    }
     return (
-        <div>Selected Pokemon: {selectedPokemon}</div>
+        <div>Selected Pokemon: {selectedPokemon.charAt(0).toUpperCase() + selectedPokemon.slice(1)} {pokemonData?<img src={pokemonData.sprites.front_default} alt="Default front facing sprite for pokemon" width={360} height={360}/>:<p>Something went wrong. Pokedex entry was not loaded properly.</p>}</div>
     )
 }
 
