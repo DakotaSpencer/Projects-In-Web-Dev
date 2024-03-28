ef4a14d70e2cd9ff28ed8b1cf6c9fd276950103e
DarkRose2021 <89149647+DarkRose2021@users.noreply.github.com>
2024-03-27 22:00:32 -0600

    Started the design
    Got the things that I need on the page, working on designing it to the layout

--- frontend/pokesite/src/pages/Login.js
+++ frontend/pokesite/src/pages/Login.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+	return (
+		<div className="login">
+			<div className="borderContainer">
+				<h1>Login</h1>
+				<div>
+					<div className="inputs">
+						<label htmlFor="email">Username / Email</label>
+						<br />
+						<input
+							name="email"
+							type="text"
+							placeholder="Username / Email"
+						/>
+						<br />
+						<label htmlFor="password">Password</label>
+						<br />
+						<input
+							name="password"
+							type="password"
+							placeholder="Password"
+						/>
+						<br />
+					</div>
+					<div className="buttons">
+						<button>Signup</button>
+						<button>Login</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default Login
+export default Login;
