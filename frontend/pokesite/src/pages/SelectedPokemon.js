3a8dfaa82ca6cb8278740c965a8f71ed493193b3
Dakota <dakota.spencer0327@gmail.com>
2024-03-28 20:36:20 -0600

    Woot!! currently everything displays correctly!

--- frontend/pokesite/src/pages/SelectedPokemon.js
+++ frontend/pokesite/src/pages/SelectedPokemon.js
@@ -19,20 +19,35 @@ const SelectedPokemon = () => {
                     axios.get(`https://pokeapi.co/api/v2/pokemon-species/${selectedPokemon}`)
                     ])
                     .then(axios.spread((pokemonAxiosData, descriptionAxiosResults) => {
-                    // output of req.
-                    console.log('pokemonData:', pokemonAxiosData, 'descriptionResults:', descriptionAxiosResults)
-                    setPokemonData(pokemonAxiosData.data);
-                    setSpeciesData(descriptionAxiosResults);
-                    setLoading(false);
+                        console.log('pokemonData:', pokemonAxiosData, 'descriptionResults:', descriptionAxiosResults)
+                        setPokemonData(pokemonAxiosData.data);
+                        setSpeciesData(descriptionAxiosResults);
+                        setLoading(false);
                     }));
-            }   
+            }
+        console.log("species data: ", speciesData)
         if (pokemonData === null || pokemonData === undefined) {
             fetchPokemonData();
         }
         else{
             console.log(pokemonData)
         }
-    },[queryParameters, selectedPokemon, pokemonData])
+    },[queryParameters, selectedPokemon, pokemonData, speciesData])
+
+    useEffect(() => {
+        if (speciesData) {
+            console.log("SD:Check", speciesData)
+            var tempArr = [];
+            speciesData?.data?.flavor_text_entries?.forEach(element => {
+                if (element?.language?.name === "en"){
+                    tempArr.push(element)
+                }
+            });
+            console.log(tempArr)
+            setFlavorText(tempArr[Math.floor(Math.random()*tempArr.length)]);
+        }
+    }, [speciesData, flavorText])
+    
     if (isLoading) {
         return <div className="App">Loading...</div>;
     }else{
@@ -47,16 +62,21 @@ const SelectedPokemon = () => {
                         </h2>
                         <section className='pokemonTypes'>
                             <h3>Types:
-                                {" " + 
+                                { 
                                     pokemonData.types?.map(type => (
-                                        type.type.name?.charAt(0).toUpperCase() + type.type.name?.slice(1)
-                                    ))
+                                        " " +type.type.name?.charAt(0).toUpperCase() + type.type.name?.slice(1)+ " "
+                                    )) 
                                 }
                             </h3>
                         </section>
                         <section className='pokemonImage'>
                             {
                                 <img src={pokemonData.sprites?.front_default} alt="Default front facing sprite for pokemon" width={360} height={360}/>
+                            }
+                        </section>
+                        <section className='flavoeText'>
+                            {
+                                flavorText?.flavor_text
                             }
                         </section>
                     </div>
