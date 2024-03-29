a687274b158874eb67c3f2af88969fcde80a174f
Dakota <dakota.spencer0327@gmail.com>
2024-03-29 17:17:38 -0600

    Updated Pokemon page and create card
    Created a card to display minimal information about a pokemon. Also added functionality to the Landing page to get multiple pokemon. This logic should be roughly the same for the Search page as well.

--- frontend/pokesite/src/pages/Landing.js
+++ frontend/pokesite/src/pages/Landing.js
@@ -1,8 +1,68 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import PokemonCard from '../components/PokemonCard';
 
 const Landing = () => {
+    const [pokemonData, setPokemonData] = useState();
+    const [pokemonArray, setPokemonArray] = useState();
+    const [limit, setLimit] = useState(20);
+    let tempArray = [];
+    useEffect(() => {
+      if (pokemonData === null || pokemonData === undefined) {
+          fetchPokemonData();
+      }
+      else{
+        pokemonData?.results.forEach(pokemon => {
+          if(pokemon?.url){
+            sendToArrayAndProcess(pokemon?.url)
+          }
+        });
+      }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[pokemonData])
+
+  const fetchPokemonData = async () => {
+    axios.all([
+        axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${limit?limit:20}$offset=0`)
+        ])
+        .then(axios.spread((pokemonAxiosData) => {
+            setPokemonData(pokemonAxiosData.data);
+        }));
+  }
+
+  const sendToArrayAndProcess = async (pokemonURL) => {
+    axios.all([
+      axios.get(pokemonURL)
+      ])
+      .then(axios.spread((pokemonURLData) => {
+          tempArray.push(pokemonURLData)
+          if(tempArray.length % 20 === 0){
+            setPokemonArray(tempArray)
+          }
+      }));
+  }
+    const handleLimitButtonClick = () => {
+      setLimit(limit + 20);
+      fetchPokemonData();
+      pokemonData?.results.forEach(pokemon => {
+          if(pokemon?.url){
+            sendToArrayAndProcess(pokemon?.url)
+          }
+        });
+    }
   return (
-    <div>Landing</div>
+    <>
+      <h1>List of Pokemon:</h1>
+      <div>
+      {
+          pokemonArray?.map(pokemon => (
+              <PokemonCard pokemon={pokemon.data}/>
+              
+          ))
+      }
+      </div>
+      <button onClick={handleLimitButtonClick}>Show More</button>
+    </>
   )
 }
 
