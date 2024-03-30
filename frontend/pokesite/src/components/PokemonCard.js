e89188881ec4bab4c18ad1104ccee4d638cdb5ea
Dakota <dakota.spencer0327@gmail.com>
2024-03-30 17:55:20 -0600

    WENT SICKO MODE
    refactored the pokemonCard to take in just the name and then call the API from there. Also got Evolution Chain data!

--- frontend/pokesite/src/components/PokemonCard.js
+++ frontend/pokesite/src/components/PokemonCard.js
@@ -1,27 +1,45 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
-const PokemonCard = (props) => {   
+const PokemonCard = (props) => {  
+    const [pokemonData, setPokemonData] = useState();
+    useEffect(() => {
+        const fetchPokemonData = async () => {
+                axios.all([
+                    axios.get(`https://pokeapi.co/api/v2/pokemon/${props.pokemon}`)
+                    ])
+                    .then(axios.spread((pokemonAxiosData) => {
+                        setPokemonData(pokemonAxiosData.data);
+                    }));
+            }
+        if (pokemonData === null || pokemonData === undefined) {
+            fetchPokemonData();
+        }
+        else{
+            //console.log(pokemonData)
+        }
+    },[props.pokemon, pokemonData])
+    
     return (
         <div>
-            {props.pokemon !==null && props.pokemon!==undefined?
+            {pokemonData !==null && pokemonData!==undefined?
                 <div>
-                    <a href={`/pokemon?pokemon=${props.pokemon.id}`}>
+                    <a href={`/pokemon?pokemon=${pokemonData.id}`}>
                     <p className='pokemonName'>
-                        
                         {
-                            "#"+props.pokemon.id + " - " + props.pokemon.name?.charAt(0).toUpperCase() + props.pokemon.name?.slice(1)
+                            "#"+pokemonData.id + " - " + pokemonData.name?.charAt(0).toUpperCase() + pokemonData.name?.slice(1)
                         }
                     </p>
                     </a>
                     <section className='pokemonImage'>
                         {
-                            <img src={props.pokemon.sprites?.other.showdown.front_default} alt="showdown state animation for the pokemon" height={"auto"} width={120}/>
+                            <img src={pokemonData.sprites?.other.showdown.front_default} alt="showdown state animation for the pokemon" height={"auto"} width={120}/>
                         }
                     </section>
                     <section className='pokemonTypes'>
                         <p>
                             {
-                                props.pokemon.types?.map(type => (
+                                pokemonData.types?.map(type => (
                                     " " + type.type.name?.charAt(0).toUpperCase() + type.type.name?.slice(1)
                                 )) + " "
                             }
