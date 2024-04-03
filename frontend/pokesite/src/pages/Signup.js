98839a240dbdca0d6dfced19e7a12503be851dd9
DarkRose2021 <89149647+DarkRose2021@users.noreply.github.com>
2024-04-03 10:40:33 -0600

    finished the signup page

--- frontend/pokesite/src/pages/Signup.js
+++ frontend/pokesite/src/pages/Signup.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useState } from "react";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
-}
+	const [showPassword, setShowPassword] = useState(false);
 
-export default Signup
+	return (
+		<div className="signup">
+			<div className="borderContainer">
+				<h1>Signup</h1>
+				<div className="form">
+					<div className="inputs">
+						<label htmlFor="username">Username</label>
+						<br />
+						<input name="username" type="text" placeholder="Username" />
+						<br />
+            <label htmlFor="email">Email</label>
+						<br />
+						<input name="email" type="text" placeholder="Email" />
+						<br />
+						<label htmlFor="password">Password</label>
+						<br />
+						<input
+							name="password"
+							type={showPassword ? "text" : "password"}
+							placeholder="Password"
+						/>
+						<i
+							className={`bi bi-eye${showPassword ? "-slash-fill" : "-fill"}`}
+							onClick={() => setShowPassword(!showPassword)}
+						></i>
+						<br />
+            <label htmlFor="password">Confirm Password</label>
+						<br />
+						<input
+							name="password"
+							type={showPassword ? "text" : "password"}
+							placeholder="Password"
+						/>
+						<i
+							className={`bi bi-eye${showPassword ? "-slash-fill" : "-fill"}`}
+							onClick={() => setShowPassword(!showPassword)}
+						></i>
+						<br />
+					</div>
+					<div className="buttons">
+						<button className="signup">Login</button>
+            <button className="loginBtn">Signup</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Signup;
