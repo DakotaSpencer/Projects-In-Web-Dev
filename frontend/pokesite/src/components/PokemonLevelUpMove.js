91ed981f14267f198842ad4f935c08f893540d09
DarkRose2021 <89149647+DarkRose2021@users.noreply.github.com>
2024-04-15 10:23:41 -0600

    worked on styling

--- frontend/pokesite/src/components/PokemonLevelUpMove.js
+++ frontend/pokesite/src/components/PokemonLevelUpMove.js
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import "./PokemonLevelUpMove.scss";
 
-const PokemonLevelUpMove = ({move}) => {
-  return (
-    <div>
-      <h5 className='move'>{
-        move.move.name?.charAt(0).toUpperCase() +
-        move.move.name?.slice(1)
-      }</h5>
-      <p>Learned at level {move.version_group_details[0]?.level_learned_at}</p>
-    </div>
-  )
-}
+const PokemonLevelUpMove = ({ move }) => {
+	const [type, setType] = useState("");
 
-export default PokemonLevelUpMove
+	useEffect(() => {
+		function getMoveType() {
+			fetch(move.move.url)
+				.then((data) => data.json())
+				.then((data) => {
+					setType(data.type.name);
+				})
+				.catch((err) => console.log(err));
+		}
+
+		getMoveType();
+	}, [move]);
+
+
+	return (
+		<div className={`move ${type}`}>
+			<h5>
+				{move.move.name?.charAt(0).toUpperCase() + move.move.name?.slice(1)}
+			</h5>
+			<p>Learned at level {move.version_group_details[0]?.level_learned_at}</p>
+		</div>
+	);
+};
+
+export default PokemonLevelUpMove;
--- frontend/pokesite/src/pages/SelectedPokemon.js
+++ frontend/pokesite/src/pages/SelectedPokemon.js
@@ -13,9 +13,9 @@ import {
 	Title,
 	Tooltip,
 	Legend,
-  } from 'chart.js';
-  import { Bar } from 'react-chartjs-2';
-  import ChartDataLabels from 'chartjs-plugin-datalabels';
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+import ChartDataLabels from "chartjs-plugin-datalabels";
 
 const SelectedPokemon = () => {
 	const [queryParameters] = useSearchParams();
@@ -25,12 +25,11 @@ const SelectedPokemon = () => {
 	const [flavorText, setFlavorText] = useState("");
 	const [evolutionChain, setEvolutionChain] = useState();
 	const [isLoading, setLoading] = useState(true);
-	const [levelUpMoves, setLevelUpMoves] = useState([])
+	const [levelUpMoves, setLevelUpMoves] = useState([]);
 	const [learnedMoves, setLearnedMoves] = useState([]);
 	const [useImperialWeight, setUseImperialWeight] = useState(true);
 	const [useImperialHeight, setUseImperialHeight] = useState(true);
 
-	
 	ChartJS.register(
 		CategoryScale,
 		LinearScale,
@@ -40,17 +39,23 @@ const SelectedPokemon = () => {
 		Legend
 	);
 
-	const delay = ms => new Promise(res => setTimeout(res, ms));
+	const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 	useEffect(() => {
 		setSelectedPokemon(queryParameters.get("pokemon"));
-		
+
 		const fetchPokemonData = async () => {
-			await delay(1050)
+			await delay(1050);
 			axios
 				.all([
-					axios.get(`https://pokeapi.co/api/v2/pokemon/${selectedPokemon?selectedPokemon:queryParameters.get("pokemon")}`),
 					axios.get(
-						`https://pokeapi.co/api/v2/pokemon-species/${selectedPokemon?selectedPokemon:queryParameters.get("pokemon")}`
+						`https://pokeapi.co/api/v2/pokemon/${
+							selectedPokemon ? selectedPokemon : queryParameters.get("pokemon")
+						}`
+					),
+					axios.get(
+						`https://pokeapi.co/api/v2/pokemon-species/${
+							selectedPokemon ? selectedPokemon : queryParameters.get("pokemon")
+						}`
 					),
 				])
 				.then(
@@ -92,30 +97,38 @@ const SelectedPokemon = () => {
 				setFlavorText(tempArr[Math.floor(Math.random() * tempArr.length)]);
 			}
 		}
-		function getMoves(){
-			let levelUpMovesArray=[];
+		function getMoves() {
+			let levelUpMovesArray = [];
 			let nonLevelUpMovesArray = [];
-			pokemonData?.moves.forEach(move => {
-				if(move.version_group_details[0].move_learn_method.name==="level-up"){
-					levelUpMovesArray.push(move)
-				}else{
-					nonLevelUpMovesArray.push(move)
+			pokemonData?.moves.forEach((move) => {
+				if (
+					move.version_group_details[0].move_learn_method.name === "level-up"
+				) {
+					levelUpMovesArray.push(move);
+				} else {
+					nonLevelUpMovesArray.push(move);
 				}
 			});
 
 			const shuffled = levelUpMovesArray.sort(() => 0.5 - Math.random());
 			let selected = shuffled.slice(0, 5);
 
-			setLevelUpMoves(selected)
-			setLearnedMoves(nonLevelUpMovesArray)
-			levelUpMovesArray.sort((a, b) => a.version_group_details[0]?.level_learned_at > b.version_group_details[0]?.level_learned_at  ? 1 : -1)
-			nonLevelUpMovesArray.sort((a, b) => a.move.name > b.move.name? 1: -1);
+			setLevelUpMoves(selected);
+			setLearnedMoves(nonLevelUpMovesArray);
+			levelUpMovesArray.sort((a, b) =>
+				a.version_group_details[0]?.level_learned_at >
+				b.version_group_details[0]?.level_learned_at
+					? 1
+					: -1
+			);
+			nonLevelUpMovesArray.sort((a, b) => (a.move.name > b.move.name ? 1 : -1));
 			learnedMoves.entries();
 		}
-		fetchData()
-		getMoves()
+		fetchData();
+		getMoves();
 	}, [speciesData]);
 
+	// Still here due to chart
 	const getTypeColor = (type) => {
 		switch (type) {
 			case "normal":
@@ -163,57 +176,93 @@ const SelectedPokemon = () => {
 
 	const getGeneration = (generation) => {
 		const romanNumerals = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 }; // Roman numeral values
-    	const match = generation.match(/generation-([ivxlcdm]+)/i); // Match Roman numerals
-    	if (!match) return null; // If input doesn't match
-		return "Generation: " + match[1].toUpperCase().split('').reduce((acc, curr, i, arr) => acc + (romanNumerals[curr] < romanNumerals[arr[i + 1]] ? -romanNumerals[curr] : romanNumerals[curr]), 0);
-	}
+		const match = generation.match(/generation-([ivxlcdm]+)/i); // Match Roman numerals
+		if (!match) return null; // If input doesn't match
+		return (
+			"Generation: " +
+			match[1]
+				.toUpperCase()
+				.split("")
+				.reduce(
+					(acc, curr, i, arr) =>
+						acc +
+						(romanNumerals[curr] < romanNumerals[arr[i + 1]]
+							? -romanNumerals[curr]
+							: romanNumerals[curr]),
+					0
+				)
+		);
+	};
 
 	const options = {
 		responsive: true,
 		scales: {
 			y: {
-				ticks: { color: '#e5e5e5ff', beginAtZero: true },
+				ticks: { color: "#e5e5e5ff", beginAtZero: true },
 				suggestedMin: 50,
-				suggestedMax: 225
+				suggestedMax: 225,
+				gridLines: {
+					color: "#e5e5e5ff",
+				},
 			},
-			x:{
-				ticks: { color: '#e5e5e5ff', beginAtZero: true },
-			}
-		}
+			x: {
+				ticks: { color: "#e5e5e5ff", beginAtZero: true },
+				gridLines: {
+					color: "#e5e5e5ff",
+				},
+			},
+		},
 	};
-	
-	const labels = ['HP', 'ATK', 'DEF', 'Special ATK', 'Special DEF', 'Speed'];
-	
+
+	const labels = ["HP", "ATK", "DEF", "Special ATK", "Special DEF", "Speed"];
+
 	const data = {
 		labels,
 		datasets: [
 			{
-				label: `${pokemonData?.name?.charAt(0).toUpperCase() +
-					pokemonData?.name?.slice(1)}'s Stats`,
-				data: [pokemonData?.stats[0].base_stat, pokemonData?.stats[1].base_stat, pokemonData?.stats[2].base_stat, pokemonData?.stats[3].base_stat, pokemonData?.stats[4].base_stat, pokemonData?.stats[5].base_stat],
+				label: `${
+					pokemonData?.name?.charAt(0).toUpperCase() +
+					pokemonData?.name?.slice(1)
+				}'s Stats`,
+				data: [
+					pokemonData?.stats[0].base_stat,
+					pokemonData?.stats[1].base_stat,
+					pokemonData?.stats[2].base_stat,
+					pokemonData?.stats[3].base_stat,
+					pokemonData?.stats[4].base_stat,
+					pokemonData?.stats[5].base_stat,
+				],
 				backgroundColor: getTypeColor(pokemonData?.types[0].type.name),
 				datalabels: {
 					color: "e5e5e5ff",
-					align: 'start',
-					anchor: 'end'
-				}
-			}
-		]
+					align: "start",
+					anchor: "end",
+				},
+			},
+		],
 	};
 
-
 	if (isLoading) {
-		return <div>
-			<div className="loader">
+		return (
+			<div>
+				<div className="loader"></div>
+				<h3
+					style={{
+						marginTop: "10%",
+						textAlign: "center",
+						fontWeight: "lighter",
+					}}
+				>
+					Fetching Pokemon, please wait...
+				</h3>
 			</div>
-			<h3 style={{marginTop: "10%", textAlign:"center", fontWeight:"lighter"}}>Fetching Pokemon, please wait...</h3>
-		</div>;
+		);
 	} else {
 		return (
 			<div className="pokemonContainer">
 				{pokemonData !== null && pokemonData !== undefined ? (
 					<div>
-						<div className="pokeInfo">
+						<section className="pokeInfo">
 							<div className="mainPokemonInfo">
 								<div className="inline">
 									<h2 className="pokemonName">
@@ -227,10 +276,7 @@ const SelectedPokemon = () => {
 										{pokemonData.types?.map((type) => (
 											<p
 												key={type.type.name}
-												className="pokemonType"
-												style={{
-													backgroundColor: getTypeColor(type.type.name),
-												}}
+												className={`pokemonType ${type.type.name}`}
 											>
 												{" " +
 													type.type.name?.charAt(0).toUpperCase() +
@@ -265,81 +311,143 @@ const SelectedPokemon = () => {
 							<section className="statisticsContainer">
 								<h1>Stats</h1>
 								<div className="pokemonStats">
-								<div className="pokemonHW">
-									<div onClick={() => setUseImperialHeight(useImperialHeight => !useImperialHeight)} className="changeSystem">{
-										useImperialHeight?
-										<p><b>Height</b>: {parseInt(((pokemonData.height * 10)/30.48))} ft {Math.round(((pokemonData.height * 10)/2.54) % 12)} in.</p>
-										:<p><b>Height</b>: {parseInt(((pokemonData.height / 10)))} meters</p>
-										}</div>
+									<div className="pokemonHW">
+										<div
+											onClick={() =>
+												setUseImperialHeight(
+													(useImperialHeight) => !useImperialHeight
+												)
+											}
+											className="changeSystem"
+										>
+											{useImperialHeight ? (
+												<p>
+													<b>Height</b>:{" "}
+													{parseInt((pokemonData.height * 10) / 30.48)} ft{" "}
+													{Math.round(((pokemonData.height * 10) / 2.54) % 12)}{" "}
+													in.
+												</p>
+											) : (
+												<p>
+													<b>Height</b>: {parseInt(pokemonData.height / 10)}{" "}
+													meters
+												</p>
+											)}
+										</div>
 
-										<div onClick={() => setUseImperialWeight(useImperialWeight => !useImperialWeight)} className="changeSystem">{
-											useImperialWeight===true?
-											<p><b>Weight</b>: {parseInt(((pokemonData.weight * 100)/28.35)/16)} lbs {Math.round(((pokemonData.weight * 100)/28.35)%16)} oz.</p>
-											:<p><b>Weight</b>: {parseInt(((pokemonData.weight / 100)))} kg</p>
-										}</div>
+										<div
+											onClick={() =>
+												setUseImperialWeight(
+													(useImperialWeight) => !useImperialWeight
+												)
+											}
+											className="changeSystem"
+										>
+											{useImperialWeight === true ? (
+												<p>
+													<b>Weight</b>:{" "}
+													{parseInt((pokemonData.weight * 100) / 28.35 / 16)}{" "}
+													lbs{" "}
+													{Math.round(
+														((pokemonData.weight * 100) / 28.35) % 16
+													)}{" "}
+													oz.
+												</p>
+											) : (
+												<p>
+													<b>Weight</b>: {parseInt(pokemonData.weight / 100)} kg
+												</p>
+											)}
+										</div>
 									</div>
-									<Bar options={options} data={data} plugins={[ChartDataLabels]} className="pokemonChart"/>
+									<Bar
+										options={options}
+										data={data}
+										plugins={[ChartDataLabels]}
+										className="pokemonChart"
+									/>
 								</div>
 							</section>
-						</div>
+						</section>
 						<section className="evolutionChainContainer">
-						{evolutionChain ? (
-							<>
-							<h3>Evolution Chain: </h3><div className="evolutionChain">
-								<PokemonCard
-									pokemon={evolutionChain?.data.chain.species.name} />
-								{evolutionChain?.data?.chain?.evolves_to[0]?.species
-									?.name ? (
-									<>
+							{evolutionChain ? (
+								<>
+									<h3>Evolution Chain: </h3>
+									<div className="evolutionChain">
 										<PokemonCard
-											pokemon={evolutionChain?.data?.chain?.evolves_to[0]?.species
-												?.name} />
-									</>
-								) : (
-									<></>
-								)}
-								{evolutionChain?.data?.chain?.evolves_to[0]?.evolves_to[0]
-									?.species?.name ? (
-									<>
-										<PokemonCard
-											pokemon={evolutionChain?.data?.chain?.evolves_to[0]
-												?.evolves_to[0]?.species?.name} />
-									</>
-								) : (
-									<></>
-								)}
-							</div></>
+											pokemon={evolutionChain?.data.chain.species.name}
+										/>
+										{evolutionChain?.data?.chain?.evolves_to[0]?.species
+											?.name ? (
+											<>
+												<PokemonCard
+													pokemon={
+														evolutionChain?.data?.chain?.evolves_to[0]?.species
+															?.name
+													}
+												/>
+											</>
+										) : (
+											<></>
+										)}
+										{evolutionChain?.data?.chain?.evolves_to[0]?.evolves_to[0]
+											?.species?.name ? (
+											<>
+												<PokemonCard
+													pokemon={
+														evolutionChain?.data?.chain?.evolves_to[0]
+															?.evolves_to[0]?.species?.name
+													}
+												/>
+											</>
+										) : (
+											<></>
+										)}
+									</div>
+								</>
 							) : (
 								<p>Loading Evolution Chain...</p>
 							)}
 						</section>
 						<section className="pokemonAbilities">
-							
-							{pokemonData?
-								
+							{pokemonData ? (
 								<>
 									<h3>Abilities ({pokemonData?.abilities.length})</h3>
 									<div className="abilitiesContainer">
-										{pokemonData?pokemonData?.abilities?.map((ability) => (
-											<PokemonAbility ability={ability.ability} key={ability.ability.name}/>
-										)):""}
+										{pokemonData
+											? pokemonData?.abilities?.map((ability) => (
+													<PokemonAbility
+														ability={ability.ability}
+														key={ability.ability.name}
+													/>
+											  ))
+											: ""}
 									</div>
 								</>
-							:<></>}
+							) : (
+								<></>
+							)}
 						</section>
 						<section className="pokemonMoves">
-						{
-								levelUpMoves?
+							{levelUpMoves ? (
 								<>
-									<h3>Moves Learned through Level Ups ({levelUpMoves.length})</h3>
+									<h3>
+										Moves Learned through Level Ups ({levelUpMoves.length})
+									</h3>
 									<div className="movesContainer">
-										{levelUpMoves?levelUpMoves?.map((move) => (
-											<PokemonLevelUpMove move={move} key={move.move.name}></PokemonLevelUpMove>
-										)):""}
+										{levelUpMoves
+											? levelUpMoves?.map((move) => (
+													<PokemonLevelUpMove
+														move={move}
+														key={move.move.name}
+													></PokemonLevelUpMove>
+											  ))
+											: ""}
 									</div>
 								</>
-								:<></>
-							}
+							) : (
+								<></>
+							)}
 						</section>
 						{/* <section className="pokemonMoves">
 							{learnedMoves?
