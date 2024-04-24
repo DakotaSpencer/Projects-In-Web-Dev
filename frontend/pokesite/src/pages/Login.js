df57b0de687f45bc574a143d7fc3df49a4996f96
DarkRose2021 <89149647+DarkRose2021@users.noreply.github.com>
2024-04-24 09:49:31 -0600

    worked on validation

--- frontend/pokesite/src/pages/Login.js
+++ frontend/pokesite/src/pages/Login.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { ErrorMessage } from "@hookform/error-message";
+import { useForm } from "react-hook-form";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 const Login = () => {
@@ -10,28 +12,81 @@ const Login = () => {
 		navigate(path);
 	};
 
+	const {
+		register,
+		formState: { errors },
+		handleSubmit,
+	} = useForm({
+		criteriaMode: "all",
+	});
+
+	const onSubmit = (data) =>{
+		// fetch statement for the backend
+		console.log(data)
+	}
+
 	return (
 		<div className="login">
 			<div className="borderContainer">
 				<h1>Login</h1>
-				<div className="form">
+				<form className="form" onSubmit={handleSubmit(onSubmit)}>
 					<div className="inputs">
-						<label htmlFor="email">Username / Email</label>
-						<br />
-						<input name="email" type="text" placeholder="Username / Email" />
-						<br />
-						<label htmlFor="password">Password</label>
-						<br />
+						<label htmlFor="username">Username <span className="error required">*</span></label>
+						<input
+							name="username"
+							type="text"
+							placeholder="Username"
+							{...register("username", {
+								required: "Username is required",
+								pattern: {
+									value: /^[a-zA-Z0-9_-]{3,16}$/i,
+									message: "Username is invalid",
+								},
+							})}
+						/>
+						<ErrorMessage
+							errors={errors}
+							name="email"
+							render={({ messages }) =>
+								messages
+									? Object.entries(messages).map(([type, message]) => (
+											<p key={type} className="error">
+												{message}
+											</p>
+									  ))
+									: null
+							}
+						/>
+						<label htmlFor="password">Password <span className="error required">*</span></label>
 						<input
 							name="password"
 							type={showPassword ? "text" : "password"}
 							placeholder="Password"
+							{...register("password", {
+								required: "Password is required"
+							})}
 						/>
 						<i
 							className={`bi bi-eye${showPassword ? "-slash-fill" : "-fill"}`}
 							onClick={() => setShowPassword(!showPassword)}
 						></i>
-						<br />
+						<ErrorMessage
+							errors={errors}
+							name="password"
+							render={({ messages }) =>
+								messages
+									? Object.entries(messages).map(([type, message]) => (
+											<p
+												key={type}
+												className="error"
+												style={{ whiteSpace: "pre-line" }}
+											>
+												{message}
+											</p>
+									  ))
+									: null
+							}
+						/>
 						<p className="forgot">
 							<a href="/home">Forgot Password</a>
 						</p>
@@ -42,7 +97,7 @@ const Login = () => {
 						</button>
 						<button className="loginBtn">Login</button>
 					</div>
-				</div>
+				</form>
 			</div>
 		</div>
 	);
--- frontend/pokesite/src/pages/Signup.js
+++ frontend/pokesite/src/pages/Signup.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { useNavigate } from "react-router";
+import { ErrorMessage } from "@hookform/error-message";
+import { useForm } from "react-hook-form";
 
 const Signup = () => {
 	const [showPassword, setShowPassword] = useState(false);
@@ -9,45 +11,176 @@ const Signup = () => {
 		let path = `/login`;
 		navigate(path);
 	};
+	const [passwordMatch, setPasswordMatch] = useState();
+
+	const {
+		register,
+		formState: { errors },
+		handleSubmit,
+		watch,
+	} = useForm({
+		criteriaMode: "all",
+	});
+
+	const onSubmit = (data) => {
+		// fetch statement for the backend
+		console.log(data);
+	};
+
+	const passwordPattern =
+		/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#$%^&*]).{8,}$/;
+	const validatePassword = (value) => {
+		if (passwordPattern.test(value)) {
+			return true;
+		}
+		return `Password must contain: \n 1 uppercase letter \n1 lowercase letter \n 1 number \n 1 special character`;
+	};
+
+	useEffect(() => {
+		setPasswordMatch(watch("password", "") === watch("confirmPassword", ""));
+	}, [watch]);
 
 	return (
 		<div className="signup">
 			<div className="borderContainer">
 				<h1>Signup</h1>
-				<div className="form">
+				<form className="form" onSubmit={handleSubmit(onSubmit)}>
 					<div className="inputs">
 						<label htmlFor="username">Username</label>
 						<br />
-						<input name="username" type="text" placeholder="Username" />
+						<input name="username" type="text" placeholder="Username"  {...register("username", {
+								required: "Username is required",
+								pattern: {
+									value: /^[a-zA-Z0-9_-]{3,16}$/,
+									message: "Invaild username",
+								},
+								minLength: {
+									value: 3,
+									message: "Username can't be shorter than 3 characters",
+								},
+								maxLength: {
+									value: 16,
+									message: "Username cannot exceed 16 characters",
+								},
+							})}
+						/>
+						<ErrorMessage
+							errors={errors}
+							name="username"
+							render={({ messages }) =>
+								messages
+									? Object.entries(messages).map(([type, message]) => (
+											<p key={type} className="error">
+												{message}
+											</p>
+									  ))
+									: null
+							}
+						/>
+						<label htmlFor="name">Name</label>
 						<br />
-						<label htmlFor="email">Email</label>
-						<br />
-						<input name="email" type="text" placeholder="Email" />
-						<br />
+						<input
+							name="name"
+							type="text"
+							placeholder="Name"
+							{...register("name", {
+								required: "Name is required",
+								pattern: {
+									value: /^[A-Za-z\s]+$/,
+									message: "Name can only contain letters",
+								},
+								minLength: {
+									value: 2,
+									message: "Name can't be shorter than 2 characters",
+								},
+								maxLength: {
+									value: 100,
+									message: "Name cannot exceed 100 characters",
+								},
+							})}
+						/>
+						<ErrorMessage
+							errors={errors}
+							name="name"
+							render={({ messages }) =>
+								messages
+									? Object.entries(messages).map(([type, message]) => (
+											<p key={type} className="error">
+												{message}
+											</p>
+									  ))
+									: null
+							}
+						/>
 						<label htmlFor="password">Password</label>
 						<br />
 						<input
 							name="password"
 							type={showPassword ? "text" : "password"}
 							placeholder="Password"
+							{...register("password", {
+								required: "Password is required",
+								validate: validatePassword,
+								minLength: {
+									value: 8,
+									message: "Must be longer than 8 characters",
+								},
+							})}
 						/>
 						<i
 							className={`bi bi-eye${showPassword ? "-slash-fill" : "-fill"}`}
 							onClick={() => setShowPassword(!showPassword)}
 						></i>
-						<br />
-						<label htmlFor="password">Confirm Password</label>
+						<ErrorMessage
+							errors={errors}
+							name="password"
+							render={({ messages }) =>
+								messages
+									? Object.entries(messages).map(([type, message]) => (
+											<p
+												key={type}
+												className="error"
+												style={{ whiteSpace: "pre-line" }}
+											>
+												{message}
+											</p>
+									  ))
+									: null
+							}
+						/>
+						
+						<label htmlFor="confirmPassword">Confirm Password</label>
 						<br />
 						<input
-							name="password"
+							name="confirmPassword"
 							type={showPassword ? "text" : "password"}
-							placeholder="Password"
+							placeholder="Confirm Password"
+							{...register("confirmPassword", {
+								required: "Confirm Password is required",
+								validate: (value) =>
+									value === watch("password", "") || "Passwords do not match",
+							})}
 						/>
 						<i
 							className={`bi bi-eye${showPassword ? "-slash-fill" : "-fill"}`}
 							onClick={() => setShowPassword(!showPassword)}
 						></i>
-						<br />
+						{passwordMatch ? null : (
+							<p className="error">Passwords do not match</p>
+						)}
+						<ErrorMessage
+							errors={errors}
+							name="confirmPassword"
+							render={({ messages }) =>
+								messages
+									? Object.entries(messages).map(([type, message]) => (
+											<p key={type} className="error">
+												{message}
+											</p>
+									  ))
+									: null
+							}
+						/>
 					</div>
 					<div className="buttons">
 						<button className="signup" onClick={routeChange}>
@@ -55,7 +188,7 @@ const Signup = () => {
 						</button>
 						<button className="loginBtn">Signup</button>
 					</div>
-				</div>
+				</form>
 			</div>
 		</div>
 	);
