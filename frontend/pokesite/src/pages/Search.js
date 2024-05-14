6cbedf7dbb4ba818908a3bf538233cf94066893f
DakotaSpencer <dakota.spencer0327@gmail.com>
2024-05-13 23:18:10 -0600

    Update Search.js

--- frontend/pokesite/src/pages/Search.js
+++ frontend/pokesite/src/pages/Search.js
@@ -5,12 +5,30 @@ import { useSearchParams } from 'react-router-dom';
 const Search = () => {
     const [searchTerm] = useSearchParams()
     const [searchQuery, setSearchQuery] = useState('')
+    const [pokemonArray, setPokemonArray] = useState();
+    const [searchResults, setSearchResults] = useState([]);
     useEffect(() => {
         setSearchQuery(searchTerm.get("query"))
+        const fetchAllPokemon = () => {
+            //fetch all pokemon
+        }
     },[searchQuery, searchTerm])
+
+    useEffect(() => {
+        //filter pokemonArray based on Search term and store in searchResults array
+    }, [pokemonArray])
     
     return (
-        <div>Search: searched for: {searchQuery}</div>
+        <div>
+            <h2>Search results For: {searchQuery}</h2>
+            <section>
+                <div className='searchResults'>
+                    {/*Iterate through searchResults and display each result, if available. 
+                    If not, display No Reslts Found*/}
+                </div>
+            </section>
+        
+        </div>
     )
 }
 
