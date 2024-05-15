c782ff3a3bb88ff20e0d4df664e72f4b4cc646ee
DakotaSpencer <dakota.spencer0327@gmail.com>
2024-05-14 19:19:59 -0600

    begin search

--- frontend/pokesite/src/pages/Search.js
+++ frontend/pokesite/src/pages/Search.js
@@ -1,30 +1,71 @@
 import React, { useState, useEffect } from 'react'
 // import { useParams } from 'react-router';
 import { useSearchParams } from 'react-router-dom';
+import axios from 'axios';
+import PokemonCard from '../components/PokemonCard';
+import PokemonSearchResultCard from '../components/PokemonSearchResultCard';
 
 const Search = () => {
     const [searchTerm] = useSearchParams()
     const [searchQuery, setSearchQuery] = useState('')
     const [pokemonArray, setPokemonArray] = useState();
     const [searchResults, setSearchResults] = useState([]);
+    
+    
     useEffect(() => {
-        setSearchQuery(searchTerm.get("query"))
-        const fetchAllPokemon = () => {
-            //fetch all pokemon
+        setSearchQuery(searchTerm.get("query"));
+        if (pokemonArray === null || pokemonArray === undefined) {
+            fetchpokemonArray();
         }
-    },[searchQuery, searchTerm])
+        console.log(pokemonArray)
+    },[pokemonArray])
+
 
     useEffect(() => {
-        //filter pokemonArray based on Search term and store in searchResults array
+        let tempPokemonArray = []
+        pokemonArray?.results?.forEach(pokemon => {
+            //console.log(element.name)
+            //console.log("Search Query: ",searchQuery)
+            let tempName = pokemon.name.toLowerCase()
+            console.log("TempName Type: ", typeof tempName)
+            if(tempName.includes(searchQuery)){
+                
+                tempPokemonArray.push(pokemon)
+                //console.log("Pokemon: " + pokemon.name + " matches search term:" + searchQuery)
+            }else{
+                console.log("pokemon does not match")
+            }
+            setSearchResults(tempPokemonArray)
+        });
+        
+        
     }, [pokemonArray])
+
+    const fetchpokemonArray = async () => {
+        axios.all([
+                axios.get(`https://pokeapi.co/api/v2/pokemon?limit=2000$offset=0`)
+            ])
+            .then(axios.spread((pokemonAxiosData) => {
+                setPokemonArray(pokemonAxiosData.data);
+            }));
+    }
+
+    useEffect(() => {
+        console.log("Search Results:" + searchResults)
+    }, searchResults)
     
     return (
         <div>
             <h2>Search results For: {searchQuery}</h2>
             <section>
                 <div className='searchResults'>
-                    {/*Iterate through searchResults and display each result, if available. 
-                    If not, display No Reslts Found*/}
+                    {
+                        searchResults.length >=1 ? searchResults?.map(result => (
+                            <div>
+                                <PokemonSearchResultCard pokemon={result.name}/>
+                            </div>
+                        )): <div><h3>No Pokemon Found</h3></div>
+                    }
                 </div>
             </section>
         
